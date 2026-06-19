0d5f5ac2ce41146ee024f5e0a59e0150b02347fe
Karan Bhatre <bhatrekaran83@gmail.com>
2026-06-19 12:58:11 +0530

    Fix typo in formatDate function for month

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,9 +1,9 @@
 export function formatDate(dateString) {
-    return new Date(dateString).toLocaleDateString("en-IN", {
-        year: "numeric",
-        mont: "long",
-        day: "numeric",
-        hour: "2-digit",
-        minute: "2-digit",
-    });
+  return new Date(dateString).toLocaleDateString("en-IN", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
 }
